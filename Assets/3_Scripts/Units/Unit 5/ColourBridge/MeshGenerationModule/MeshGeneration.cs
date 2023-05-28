c49a893617fc780cbb33ffba410aca9e747eb28a
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-05-28 21:51:01 +0300

    +refactor: MeshGeneration

--- Assets/3_Scripts/Units/Unit 5/ColourBridge/MeshGenerationModule/MeshGeneration.cs
+++ Assets/3_Scripts/Units/Unit 5/ColourBridge/MeshGenerationModule/MeshGeneration.cs
@@ -16,20 +16,10 @@ namespace Funlary.Unit5.ColourBridge.MeshGenerationModule
             MeshFilter meshFilter = meshObject.AddComponent<MeshFilter>();
             MeshRenderer meshRenderer = meshObject.AddComponent<MeshRenderer>();
             MeshCollider meshCollider = meshObject.AddComponent<MeshCollider>();
-            Mesh mesh = BuildPlaneMesh(startPoint, endPoint, bridgeWidth, bridgeLength, bridgeHeight, meshParent);
+            Mesh mesh = BuildPlaneMesh(bridgeWidth, bridgeLength, bridgeHeight, meshParent);
 
             meshRenderer.material = meshMaterial;
             meshCollider.sharedMesh = mesh;
-            
-            /*
-            switch (meshType)
-            {
-                case MeshType.PLANE:
-                    mesh = CreatePlaneMesh(startPoint, endPoint);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(meshType), meshType, null);
-            }*/
 
             meshFilter.mesh = mesh;
             meshObject.transform.rotation = CalculateRotation(startPoint, endPoint);
@@ -41,20 +31,12 @@ namespace Funlary.Unit5.ColourBridge.MeshGenerationModule
             return Quaternion.LookRotation(direction);
         }
         
-        private Mesh BuildPlaneMesh(Transform startPoint, Transform endPoint, float bridgeWidth, float bridgeLength, float bridgeHeight, Transform meshParent, int segmentCount = 5)
+        private Mesh BuildPlaneMesh(float bridgeWidth, float bridgeLength, float bridgeHeight, Transform meshParent, int segmentCount = 5)
         {
-            // Başlangıç ve bitiş noktalarının düzlemlerini alıyoruz
-            Vector3 startPointPosition = startPoint.position;
-            Vector3 endPointPosition = endPoint.position;
-            
-            // Yükseklik değeri olarak 0 kabul ediyoruz, isterseniz farklı bir yükseklik değeri de verebilirsiniz
             float height = 0f;
-
-            // Plane'in genişlik ve uzunluk değerlerini hesaplıyoruz
             float width = bridgeWidth / 2;
             float length = Mathf.Sqrt(Mathf.Pow(bridgeLength,2) + Mathf.Pow(bridgeHeight,2));
-
-            // Plane için vertex, triangle ve uv verilerini oluşturuyoruz
+            
             Vector3[] vertices = new Vector3[4]
             {
                 new Vector3(width, 0, 0f),
@@ -76,8 +58,7 @@ namespace Funlary.Unit5.ColourBridge.MeshGenerationModule
                 new Vector2(0f, 1f),
                 new Vector2(1f, 1f)
             };
-
-            // Mesh nesnesini oluşturup verileri atıyoruz
+            
             Mesh mesh = new Mesh();
             mesh.vertices = vertices;
             mesh.triangles = triangles;
