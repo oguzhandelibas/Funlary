165b5ac158a9e6b6c0c74b13c1396fd494c3b60f
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-11-06 23:04:09 +0300

    +refactor: Color Sounds

---
+++ Assets/3_Scripts/Units/Unit 5/ColourBridge/OpponentModule/AudioControl/OpponentAudioController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Funlary.Unit5.OpponentModule
+{
+    public class OpponentAudioController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
